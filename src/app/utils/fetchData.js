b9ac9981a12336b887653e811a5fa5d324a50329
Brinda Muraleedharan <brinda.salesforce@gmail.com>
2024-07-01 21:59:49 -0400

    date issue handling

--- src/app/utils/fetchData.js
+++ src/app/utils/fetchData.js
@@ -1,31 +1,35 @@
 import dictstring from "../weathercodes";
-
-let fetchApiData=(options,dispatch,loadHourData,params,loadDailyData,setLocationName,setErrorData)=>{
+let fetchApiData=async(options,dispatch,loadHourData,params,loadDailyData,setLocationName,setErrorData)=>{
     let hourTemp=[];
     let dailyTemp=[];
     let errorValue;
+    let timeZone;
     fetch('https://api.tomorrow.io/v4/weather/forecast?apikey=PShPpkTDzWirRosXaLMlCC3v7fa0hMOi&'+params).then(
         response => {
             errorValue=response.status;
             console.log(response.status+"statusCode");
             dispatch(setErrorData(response.status));
+            
             return response.json()
         }
     ).then(
-        json=>{
+        async json=>{
             if(errorValue==200){
-                console.log(json)
-            json.timelines.daily.forEach((item) => {
+                console.log(json);
+                let res=await fetch(`http://api.geonames.org/timezoneJSON?lng=${json.location.lon}&lat=${json.location.lat}&username=bmuralee`)
+                if (!res.ok) {
+                    console.log(res.statusText);
+                  }
+                  const t = await res.json();
+                  timeZone= await t.timezoneId;
+                json.timelines.daily.forEach((item) => {
                 const date = new Date(item.time);
-                const weekday = ["Sun ","Mon ","Tue ","Wed ","Thur ","Fri ","Sat "];
+                const day=date.toDateString("en-US", {timeZone:"America/New_York",weekday: "short"});
                 let tempObj={
-                    //val:parseInt(item.values.temperature),
                     tempMax:parseInt(item.values.temperatureMax),
                     tempMin:parseInt(item.values.temperatureMin),
                     code:item.values.weatherCodeMax,
-                    //tempFeel:item.values.temperatureApparent,
-                    day:weekday[date.getDay()],
-                    //time:(date.getHours()>12?(date.getHours()-12)+"pm":date.getHours()+"am")
+                    day:day
                 }
                 tempObj.desc=dictstring[tempObj.code]
                 dailyTemp.push(tempObj);
@@ -35,11 +39,13 @@ let fetchApiData=(options,dispatch,loadHourData,params,loadDailyData,setLocation
             for(let i=0;i<6;i++){
                 let item=hourData[i]
                 const date = new Date(item.time);
+                console.log(date.toLocaleString("en-US", {timeZone:timeZone,hour: "2-digit"}))
+                const time=date.toLocaleString("en-US", {timeZone:timeZone,hour: "2-digit",hourCycle: "h24"});
                 let tempObj={
                     val:parseInt(item.values.temperature),
-                    code:(date.getHours()>18)?item.values.weatherCode+"1":item.values.weatherCode+"0",
+                    code:(time>18||time<6)?item.values.weatherCode+"1":item.values.weatherCode+"0",
                     tempFeel:item.values.temperatureApparent,
-                    time:(date.getHours()>12?(date.getHours()-12)+"pm":date.getHours()+"am")
+                    time:time
                 }
                 tempObj.desc=dictstring[tempObj.code]
               hourTemp.push(tempObj);
